6de4f69170a93ec8d6394bff611713e1ab64abc8
Sergio Rios <sergioriosp04@gmail.com>
2025-08-13 21:14:09 -0500

    add order resource for filament

---
+++ app/Filament/Resources/Orders/Schemas/OrderForm.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Filament\Resources\Orders\Schemas;
+
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
+use Filament\Schemas\Schema;
+
+class OrderForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('title')
+                    ->required(),
+                Textarea::make('body')
+                    ->required()
+                    ->columnSpanFull(),
+                TextInput::make('total')
+                    ->required()
+                    ->numeric(),
+                Select::make('status_id')
+                    ->relationship('status', 'title')
+                    ->required(),
+            ]);
+    }
+}
---
+++ app/Filament/Resources/Orders/Tables/OrdersTable.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Filament\Resources\Orders\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class OrdersTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('title')
+                    ->searchable(),
+                TextColumn::make('total')
+                    ->numeric()
+                    ->sortable(),
+                TextColumn::make('status.title')
+                    ->numeric()
+                    ->sortable(),
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
