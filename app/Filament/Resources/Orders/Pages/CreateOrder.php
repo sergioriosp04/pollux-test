c76384fb9a636756c2aede5451232fe657c14d88
Sergio Rios <sergioriosp04@gmail.com>
2025-08-15 12:00:26 -0500

    add form to create orders

--- app/Filament/Resources/Orders/Pages/CreateOrder.php
+++ app/Filament/Resources/Orders/Pages/CreateOrder.php
@@ -8,4 +8,17 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateOrder extends CreateRecord
 {
     protected static string $resource = OrderResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $totalOrder = 0;
+        if (!empty($data['orderProducts'])) {
+            $totalOrder = collect($data['orderProducts'])->map(function ($item) use ($totalOrder) {
+                return $item['total'];
+            })->sum();
+        }
+
+        $data['total'] = $totalOrder;
+        return $data;
+    }
 }
--- app/Filament/Resources/Orders/Schemas/OrderForm.php
+++ app/Filament/Resources/Orders/Schemas/OrderForm.php
@@ -2,9 +2,14 @@
 
 namespace App\Filament\Resources\Orders\Schemas;
 
+use App\Common\ProductStatesCommon;
+use Filament\Forms\Components\Hidden;
+use Filament\Forms\Components\Repeater;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Textarea;
+use Filament\Schemas\Components\Utilities\Get;
+use Filament\Schemas\Components\Utilities\Set;
 use Filament\Schemas\Schema;
 
 class OrderForm
@@ -14,16 +19,100 @@ class OrderForm
         return $schema
             ->components([
                 TextInput::make('title')
+                    ->columns(2)
+                    ->required(),
+                Select::make('status_id')
+                    ->relationship('status', 'title')
+                    ->columns(2)
                     ->required(),
                 Textarea::make('body')
                     ->required()
                     ->columnSpanFull(),
-                TextInput::make('total')
-                    ->required()
-                    ->numeric(),
-                Select::make('status_id')
-                    ->relationship('status', 'title')
-                    ->required(),
+
+                Repeater::make('orderProducts')
+                    ->columnSpanFull()
+                    ->relationship('orderProducts')
+                    ->schema([
+                        Select::make('product_id')
+                        ->relationship(
+                            name: 'product', 
+                            titleAttribute: 'title',
+                            modifyQueryUsing: fn ($query) => $query
+                                ->where('stock', '>', 0)
+                                ->where('status_id', ProductStatesCommon::ACTIVE)
+                            )
+                        ->required()
+                        ->searchable()
+                        ->live()
+                        ->columnSpan(['default' => 2, 'md' => 1])
+                        ->afterStateUpdated(function (Set $set, Get $get, $state) {
+                            if ($state) {
+                                $product = \App\Models\Product::find($state);
+                                
+                                if ($product) {
+                                    $set('price', $product->price);
+                                    $set('unit_price', $product->price);
+                                    $set('total', $product->price);
+                                    $set('total_price_display', number_format($product->price, 2));
+                                    $set('title', $product->title);
+                                    $set('stock', $product->stock);
+                                    $set('currency', $product->currency);
+                                }
+                            }
+                        }),
+
+                        TextInput::make('title')
+                            ->dehydrated()
+                            ->columnSpan(['default' => 2, 'md' => 1])
+                            ->required(),
+                        
+                        TextInput::make('quantity')
+                            ->numeric()
+                            ->default(1)
+                            ->required()
+                            ->minValue(1)
+                            ->maxValue(function (Get $get) {
+                                $stock = $get('stock');
+                                if ($stock) {
+                                    return $stock;
+                                }
+                                return 0;
+                            })
+                            ->live()
+                            ->afterStateUpdated(function (Set $set, Get $get, $state) {
+                                $stock = $get('stock');
+                                if ($state && $stock) {
+                                    $totalPrice = round((float) $state * $get('price'), 2);
+                                    $set('total', $totalPrice);
+                                    $set('total_price_display', number_format($totalPrice, 2));
+                                }
+                            })
+                            ->columnSpan(['default' => 2, 'md' => 1])
+                            ->helperText(function (Get $get) {
+                                $stock = $get('stock');
+                                if ($stock) {
+                                    return "Stock : {$stock}";
+                                }
+                                return 'First you should add a product';
+                            }),
+
+                        TextInput::make('total_price_display')
+                            ->label('Total Price')
+                            ->prefix('$')
+                            ->formatStateUsing(fn ($state) => $state ? number_format($state, 2) : '')
+                            ->columnSpan(['default' => 2, 'md' => 1])
+                            ->disabled(),
+
+                        Hidden::make('total'),
+                        Hidden::make('price'),
+                        Hidden::make('unit_price'),
+                        Hidden::make('stock'),
+                        Hidden::make('currency'),
+                    ])
+                    ->columns(2)
+                    ->dehydrated()
+                    ->defaultItems(1)
+                    ->addActionLabel('Add product')
             ]);
     }
 }
