c29a4bb19ff8a3c39c658649b3ad7f2d1156bbb4
Sergio Rios <sergioriosp04@gmail.com>
2025-08-15 14:52:27 -0500

    add logic for stock to create action

--- app/Filament/Resources/Orders/Pages/CreateOrder.php
+++ app/Filament/Resources/Orders/Pages/CreateOrder.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\Orders\Pages;
 
 use App\Filament\Resources\Orders\OrderResource;
 use Filament\Resources\Pages\CreateRecord;
+use App\Models\Order;
 
 class CreateOrder extends CreateRecord
 {
@@ -13,12 +14,27 @@ class CreateOrder extends CreateRecord
     {
         $totalOrder = 0;
         if (!empty($data['orderProducts'])) {
-            $totalOrder = collect($data['orderProducts'])->map(function ($item) use ($totalOrder) {
+            $totalOrder = collect($data['orderProducts'])->map(function ($item) {
                 return $item['total'];
             })->sum();
         }
 
         $data['total'] = $totalOrder;
         return $data;
+    }
+
+    protected function afterCreate(): void
+    {
+        /** @var Order $order */
+        $order = $this->record;
+
+        if ($order->orderProducts->isNotEmpty()) {
+            foreach ($order->orderProducts as $orderProduct) {
+                $quantity = $orderProduct->quantity;
+                $product = $orderProduct->product;
+                $product->stock -= $quantity;
+                $product->save();
+            }
+        }
     }
 }
